2afa2f6b6bd51a7f5e7f730ca45087a9d0559f79
Adik901 <adikdima29@gmail.com>
2023-02-01 11:45:01 -0500

    Мой первый коммит,

---
+++ src/Day1GitHub/FirstGitHub.java
@@ -0,0 +1,9 @@
+package Day1GitHub;
+
+public class FirstGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hey GitHUb!");
+        System.out.println(1+1);
+
+    }
+}
