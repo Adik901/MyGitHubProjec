c7e70773f2d9cbd6da231e59fd7afc3410df93fa
Adik901 <124158128+Adik901@users.noreply.github.com>
2023-02-01 18:16:10 -0500

    Update FirstGitHub.java
    helllo

--- src/Day1GitHub/FirstGitHub.java
+++ src/Day1GitHub/FirstGitHub.java
@@ -2,7 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHub {
     public static void main(String[] args) {
-        System.out.println("Hey GitHUb!");
+        System.out.println("Hellllo GitHUb!");
         System.out.println(1+1);
 
     }
