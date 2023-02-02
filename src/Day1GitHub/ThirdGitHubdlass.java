41593a3da4e01aa3f6925b1c38f346df2f7b267e
Adik901 <adikdima29@gmail.com>
2023-02-02 10:06:48 -0500

    I added a new cLass named ThirdGitHubdlass

--- src/Day1GitHub/ThirdGitHubdlass.java
+++ src/Day1GitHub/ThirdGitHubdlass.java
@@ -1,6 +1,7 @@
 package Day1GitHub;
 
-public class ThirdGitHubdlass {
+public class
+ThirdGitHubdlass {
     public static void main(String[] args) {
         System.out.println("i am writing this on DimaFeature");
     }
